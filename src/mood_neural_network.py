fe7aa9c7a35f509622b81b74b81383cbbe06824d
Rajvirsinh <rjs.jadeja@gmail.com>
2025-08-25 20:12:57 -0700

    Improved model

--- src/mood_neural_network.py
+++ src/mood_neural_network.py
@@ -1,10 +1,13 @@
 import numpy as np
+import random
 import pickle
 
 
 def relu(x):
     return np.maximum(0, x)
 
+def relu_derivative(x):
+    return (x > 0).astype(float)
 
 def softmax(x):
     numerator = np.exp(x)
@@ -13,77 +16,71 @@ def softmax(x):
 
 
 class neuralNetwork:
-    def __init__(self, inputSize, hiddenSize1, hiddenSize2, hiddenSize3, hiddenSize4, outputSize):
-        """
-        self.weight1 = self.he_init(inputSize, hiddenSize1)
-        self.bias1 = np.zeros(hiddenSize1)
-
-        self.weight2 = self.he_init(hiddenSize1, hiddenSize2)
-        self.bias2 = np.zeros(hiddenSize2)
-
-        self.weight3 = self.he_init(hiddenSize2, hiddenSize3)
-        self.bias3 = np.zeros(hiddenSize3)
-
-        self.weight4 = self.he_init(hiddenSize3, hiddenSize4)
-        self.bias4 = np.zeros(hiddenSize4)
-
-        self.weight5 = self.he_init(hiddenSize4, outputSize)
-        self.bias5 = np.zeros(outputSize)
-
-        with open("weights_biases.pkl", "wb") as file:
-            pickle.dump({
-                "weight1": self.weight1, "bias1": self.bias1,
-                "weight2": self.weight2, "bias2": self.bias2,
-                "weight3": self.weight3, "bias3": self.bias3,
-                "weight4": self.weight4, "bias4": self.bias4,
-                "weight5": self.weight5, "bias5": self.bias5
-            }, file)
-        
-        """
-        
+    def __init__(self):
         with open("data/weights_biases.pkl", "rb") as file:
             data = pickle.load(file)
-            
-        self.weight1 = np.array(data["weight1"])
-        self.bias1   = np.array(data["bias1"])
-        self.weight2 = np.array(data["weight2"])
-        self.bias2   = np.array(data["bias2"])
-        self.weight3 = np.array(data["weight3"])
-        self.bias3   = np.array(data["bias3"])
-        self.weight4 = np.array(data["weight4"])
-        self.bias4   = np.array(data["bias4"])
-        self.weight5 = np.array(data["weight5"])
-        self.bias5   = np.array(data["bias5"])
-
-
-    def he_init(self, fan_in, fan_out):
-        return np.random.randn(fan_out, fan_in) * np.sqrt(2 / fan_in)
-
-
+        
+        self.weights = [np.array(data["weight1"]), np.array(data["weight2"]), np.array(data["weight3"]), np.array(data["weight4"]), np.array(data["weight5"])]
+        self.biases = [np.array(data["bias1"]), np.array(data["bias2"]), np.array(data["bias3"]), np.array(data["bias4"]), np.array(data["bias5"])]
+    
+    
     def forward_pass(self, x) -> tuple:
-        z1 = np.dot(self.weight1, x) + self.bias1
-        a1 = relu(z1)
+        z_list = []
+        activation_list = [x]
+        
+        for i in range(len(self.weights) - 1):
+            z = np.dot(self.weights[i], activation_list[i]) + self.biases[i]
+            a = relu(z)
+            
+            z_list.append(z)
+            activation_list.append(a)
 
-        z2 = np.dot(self.weight2, a1) + self.bias2
-        a2 = relu(z2)
-
-        z3 = np.dot(self.weight3, a2) + self.bias3
-        a3 = relu(z3)
-
-        z4 = np.dot(self.weight4, a3) + self.bias4
-        a4 = relu(z4)
-
-        z5 = np.dot(self.weight5, a4) + self.bias5
+        z5 = np.dot(self.weights[-1], activation_list[-1]) + self.biases[-1]
         a5 = softmax(z5)
+        
+        z_list.append(z5)
+        activation_list.append(a5)
 
-        return z1, a1, z2, a2, z3, a3, z4, a4, z5, a5
+        return z_list, activation_list
+    
+    
+    def backpropogate(self, z_list, activation_list, target, lr=0.1):
+        updated_weights = []
+        updated_biases = []
+        
+        dz_list = [activation_list[-1] - target]
+        
+        updated_weights.append(self.weights[-1] - (lr * (np.outer(dz_list[-1], activation_list[-2]))))
+        updated_biases.append(self.biases[-1] - (lr * dz_list[-1]))
+        
+        for i in range(4, 0, -1):
+            dz = np.dot(self.weights[i].T, dz_list[-1]) * relu_derivative(z_list[i-1])
+            
+            updated_weights.insert(0, self.weights[i-1] - (lr * (np.outer(dz, activation_list[i-1]))))
+            updated_biases.insert(0, self.biases[i-1] - (lr * dz))
+            
+            dz_list.append(dz)
+        
+        self.weights = updated_weights
+        self.biases = updated_biases
 
-"""
-nn = neuralNetwork(inputSize=10000, hiddenSize1=1024, hiddenSize2=256, hiddenSize3=64, hiddenSize4=16, outputSize=5)
 
-inputBOW = np.random.randint(0, 2, size=10000)
+    def get_weights_biases(self) -> tuple:
+        return self.weights, self.biases
 
-z1, a1, z2, a2, z3, a3, z4, a4, z5, a5 = nn.forward_pass(x=inputBOW)
-print("Output probabilities:", a5)
-print("Sum:", sum(a5))
-"""
+
+if __name__ ==  "__main__":
+    nn = neuralNetwork()
+
+    inputBOW = np.random.randint(0, 2, size=10000)
+
+    z_list, activation_list = nn.forward_pass(x=inputBOW)
+    weights, biases = nn.get_weights_biases()
+    print("Output probabilities:", activation_list[-1])
+    print("Sum:", sum(activation_list[-1]))
+    print("bias pre-backprop:", biases[-1])
+    
+    nn.backpropogate(z_list=z_list, activation_list=activation_list, target=np.array([1, 0, 0, 0, 0]))
+    weights, biases = nn.get_weights_biases()
+    print("bias post-backprop:", biases[-1])
+    
